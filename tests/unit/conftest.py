88909a609a71c47beffbeb338580fc2836085a72
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-10-17 15:23:52 +0530

    Use standard test access credentials and region for unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,6 +3,12 @@ from typing import Optional
 
 import pytest
 
+from localstack.constants import (
+    TEST_AWS_ACCESS_KEY_ID,
+    TEST_AWS_REGION_NAME,
+    TEST_AWS_SECRET_ACCESS_KEY,
+)
+
 
 @pytest.fixture(autouse=True)
 def switch_region():
@@ -28,6 +34,6 @@ def set_boto_test_credentials_and_region(monkeypatch):
     """
     Automatically sets the default credentials and region for all unit tests.
     """
-    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "test")
-    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "test")
-    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", TEST_AWS_ACCESS_KEY_ID)
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", TEST_AWS_SECRET_ACCESS_KEY)
+    monkeypatch.setenv("AWS_DEFAULT_REGION", TEST_AWS_REGION_NAME)
